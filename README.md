5b3773b1df5788a581dd9d9b171ca2b41492cff8
James Hawk <jph94880@gmail.com>
2023-06-27 11:07:35 -0500

    add a form to create new articles and handle the submission of the form to send a POST request to the server.

--- README.md
+++ README.md
@@ -13,7 +13,7 @@
     - Comment
 3. [0/2] There should be at least 2 client-side routes using React RouterLinks to an external site.. Be sure to include a nav bar or other UI element that allows users to navigate between routes. Use RESTful routing conventions where applicable.
 4. [] Use a json-server to create a RESTful API for your backend and make both a GET and a POST request to the json server. 
-    - GET Request: Fetching Blog Posts, Project Showcase, Retrieving User-Generated Comments. 
+    + GET Request: Fetching Blog Posts 
     - POST Request: Users would enter their comment text and submit it, triggering a POST request to send the comment data to the server for storage and display.
 5. [] Use a form to make your post request, specifically a controlled form/component. 
 6. [] Additionally, you may choose to incorporate data from an external API but it is not required.
--- src/ArticleList.js
+++ src/ArticleList.js
@@ -1,44 +1,110 @@
 // import React from 'react';
-// import Article from './Article';
-// import Card from 'react-bootstrap/Card';
+// // import Article from './Article';
 
 // function ArticleList({ posts }) {
-//     const articleList = posts.map(post => (
-//         <Article
-//         key={post.id}
-//         title={post.title}
-//         date={post.date}
-//         preview={post.preview}
-//         />
-//     ));
-
+//     const articleList = posts.map((post) => {
+//         return (
+//             <article key={post.id}>
+//                 <h3>{post.title}</h3>
+//                 <small>{post.date}</small>
+//                 <p>{post.preview}</p>
+//             </article>
+//         )
+//     })
 //     return (
-//         <article key={posts.id}>
-//         <h3>{posts.title}</h3>
-//         <small>{posts.date}</small>
-//         <p>{posts.preview}</p>
-//     </article>
-// )};
+//         <main>
+//             {articleList}
+//         </main>
+//     )
+// }
 
-// export default ArticleList;
-
-import React from 'react';
+import React, { useState } from 'react';
 
 function ArticleList({ posts }) {
-    const articleList = posts.map((post) => {
-        return (
-            <article key={post.id}>
-                <h3>{post.title}</h3>
-                <small>{post.date}</small>
-                <p>{post.preview}</p>
-            </article>
-        )
+  const [newArticle, setNewArticle] = useState({
+    title: '',
+    date: '',
+    preview: '',
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setNewArticle((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Send POST request to the server
+    fetch('http://localhost:3000/posts', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newArticle),
     })
+      .then((response) => response.json())
+      .then((createdPost) => {
+        // Handle the response if needed
+        console.log('New article created:', createdPost);
+
+        // Clear the form inputs
+        setNewArticle({
+          title: '',
+          date: '',
+          preview: '',
+        });
+      })
+      .catch((error) => {
+        // Handle any errors that occur during the request
+        console.error('Error creating article:', error);
+      });
+  };
+
+  const articleList = posts.map((post) => {
     return (
-        <main>
-            {articleList}
-        </main>
-    )
+      <article key={post.id}>
+        <h3>{post.title}</h3>
+        <small>{post.date}</small>
+        <p>{post.preview}</p>
+      </article>
+    );
+  });
+
+  return (
+    <main>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="title"
+          value={newArticle.title}
+          onChange={handleInputChange}
+          placeholder="Title"
+        />
+        <input
+          type="text"
+          name="date"
+          value={newArticle.date}
+          onChange={handleInputChange}
+          placeholder="Date"
+        />
+        <input
+          type="text"
+          name="preview"
+          value={newArticle.preview}
+          onChange={handleInputChange}
+          placeholder="Preview"
+        />
+        <button type="submit">Add Article</button>
+      </form>
+      {articleList}
+    </main>
+  );
 }
+
+// export default ArticleList;
 
 export default ArticleList;
