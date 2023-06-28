4ef3356346557a1bdb0f0b34d59a1aa502f2ea26
James Hawk <jph94880@gmail.com>
2023-06-28 17:55:57 -0500

    Adds Home.js and About.js

--- src/Article.js
+++ src/Article.js
@@ -1,22 +1,4 @@
-// import React from "react";
-
-//     function Article({ props }) {
-//     const onHandleDelete = () => {
-//         onHandleDelete(id);
-//     }
-//     return (
-//         <article>
-//             <h3>{props.title}</h3>
-//             <small>{props.date}</small>
-//             <p>{props.preview}</p>
-//             <button onClick={onHandleDelete}>Delete</button>
-//         </article>
-//     );
-// }
-
-// export default Article;
-
-// import React from "react";
+import React from "react";
 
 function Article({ title, date = "January 1, 1970", preview }) {
 //   const handleDeleteClick = () => {
